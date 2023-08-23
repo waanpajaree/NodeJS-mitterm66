edeb56276ee8b08a9231cadc519d43e2b40faae2
waanpajaree <applekrnawee@gmail.com>
2023-08-23 13:35:45 +0700

    second commit

--- server/src/app.js
+++ server/src/app.js
@@ -9,7 +9,7 @@ app.use(bodyParser.urlencoded({extended:true}))
 require('./route')(app)
 
 app.get('/status', function (req,res){
-    res.send('Hello nodejs server belong to nattapon')
+    res.send('Hello nodejs server belong to pajaree')
 })
 
 app.get('/hello/:name', function (req,res) {
